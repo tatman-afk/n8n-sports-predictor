4aaa354eb6f1911484c5ba63c3c28edda12fc897
MaxBoving <146762130+MaxBoving@users.noreply.github.com>
2026-02-28 11:21:39 -0700

    Add shareable local backup snapshot files

---
+++ data/backups/sports_predictor_20260224_233711_globals.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+\restrict wqKxp4y9AlobuBcV3PA3nXZetzcZBS5kVQk3TQdXlZbdW3GCVdLIrWoPn0bEaCR
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE maxboving;
+ALTER ROLE maxboving WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS;
+
+--
+-- User Configurations
+--
+
+
+
+
+
+
+
+
+\unrestrict wqKxp4y9AlobuBcV3PA3nXZetzcZBS5kVQk3TQdXlZbdW3GCVdLIrWoPn0bEaCR
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
